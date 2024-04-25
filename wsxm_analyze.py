1e6f69f7b398cd000cbac6f6114f69ce4caddf8d
Pranav Sudersan <pranavsudersan@gmail.com>
2024-04-25 15:37:57 +0200

    Update Calibrator.ipynb, wsxm_analyze.py, and wsxm_read.py Dashboard for force-volume reading made: preliminary version wsxm data transformation functions separated into a different file: "wsxm_analyze.py"

---
+++ wsxm_analyze.py
@@ -0,0 +1,210 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit
+from scipy import signal
+import scipy.ndimage as ndimage
+
+def func_adhesion(force_data, zero_pts):
+    f_zero = force_data['approach']['y'][:zero_pts].mean()
+    f_min = force_data['retract']['y'].min()
+    return f_zero - f_min, {'zero': f_zero, 'min': f_min}
+
+def func_snapin(defl_data, zero_pts): #CHECK ALGORITHM!
+    segment = 'approach'
+    # defl_sobel = ndimage.sobel(defl_data[segment]['y']) #sobel transform
+    # idx_min = np.argmin(defl_sobel) #id of sharpest corner in defl data
+    defl_idx_min = np.argmin(defl_data[segment]['y']) #id of minima of data
+    # defl_snapin = defl_data[segment]['y'][idx_min]
+    z_snapin = defl_data[segment]['x'][defl_idx_min]
+    defl_min = defl_data[segment]['y'][defl_idx_min]
+    defl_zero = defl_data[segment]['y'][:zero_pts].mean()
+    # z_min = defl_data['approach']['x'][defl_idx_min]
+    # print(idx_min)
+    return defl_zero - defl_min, {'x': [z_snapin, z_snapin], 'y': [defl_zero, defl_min]}
+
+def func_stiffness(force_data, bad_pts):
+    segment = 'approach'
+    idx_min = np.argmin(force_data[segment]['y'])
+    if idx_min == force_data[segment]['x'].shape[0]-1: #when spectra not good
+        return np.inf, {'x': force_data[segment]['x'][idx_min:], 'y': force_data[segment]['y'][idx_min:]}
+    else:
+        p, res, rank, sing, rcond = np.polyfit(force_data[segment]['x'][idx_min:], 
+                                               force_data[segment]['y'][idx_min:], 1, full=True)
+        poly = np.poly1d(p)
+        fit_data = {'x': force_data[segment]['x'][idx_min:], 'y': poly(force_data[segment]['x'][idx_min:])}
+    return -p[0], fit_data
+
+#TODO: calibration dictionary to get in nm or nN from volts
+
+#dictionary of functions defined to extract spectroscopy data properties
+# FUNC_DICT = {'Adhesion': {'function':func_adhesion,
+#                           'channel': 'Normal force',
+#                           'kwargs': {'zero_pts': 10
+#                                     }
+#                          },
+#              'Stiffness': {'function':func_stiffness,
+#                            'channel': 'Normal force',
+#                            'kwargs': {'bad_pts':1}
+#                            },
+#              'Snap-in distance': {'function':func_snapin,
+#                                   'channel': 'Normal force',
+#                                   'kwargs': {}
+#                                   },
+#              }
+
+FUNC_DICT = {'Normal force': {'Adhesion': {'function':func_adhesion,
+                                           'kwargs': {'zero_pts': 10}
+                                           },
+                              'Stiffness': {'function':func_stiffness,
+                                            'kwargs': {'bad_pts':1}
+                                            },
+                              'Snap-in distance': {'function':func_snapin,
+                                                   'kwargs': {'zero_pts': 10}
+                                                   }
+                              },
+             'Amplitude': {},
+             'Excitation frequency': {},
+             'Phase': {}
+            }
+
+#rename spectroscopy line to standard names: approach and retract
+SPECT_DICT = {'Forward':'approach', 'Backward': 'retract'} 
+
+# Get spectroscopy data dictionary from force volume data at x,y
+def wsxm_getspectro(data, channel, img_dir, x=0, y=0):
+    # label_dict = {'Forward':'approach', 'Backward': 'retract'} #rename lines
+    #initialize spectroscopy data dictionary
+    # data_fd_dict = {'x': np.empty(0), 'y': np.empty(0), 'segment': np.empty(0)}
+    #spectroscopy keys for the same image direction
+    img_keys = [key for key in data[channel].keys() if key.startswith(f'Image {img_dir}')] 
+    spectro_data = {}
+    for key in img_keys:
+        spectro_dir = SPECT_DICT[key.split(' ')[3]]
+        # if segment != 'both' and segment != spectro_dir: #skip unwanted segment
+        #     continue
+        # line_pts = int(data[channel][key]['header']['Number of points per ramp'])
+        # data_fd_dict['x'] = np.append(data_fd_dict['x'], data[channel][key]['data']['Z'])
+        # data_fd_dict['y'] = np.append(data_fd_dict['y'], data[channel][key]['data']['ZZ'][:,y,x])
+        # data_fd_dict['segment'] = np.append(data_fd_dict['segment'], line_pts*[spectro_dir])
+        spectro_data[spectro_dir] = {'y': data[channel][key]['data']['ZZ'][:,y,x],
+                                     'x': data[channel][key]['data']['Z']}
+    # data_fd = pd.DataFrame.from_dict(data_fd_dict)
+    # #perform calculations for parameters (e.g. adhesion, stiffness, check FUNC_DICT) on the single spectroscopy curve
+    # data_dict_param = {}
+    # for param in FUNC_DICT[channel].keys():
+    #     kwargs = FUNC_DICT[channel][param]['kwargs']
+    #     _, data_dict_param[param] = FUNC_DICT[channel][param]['function'](spectro_data, **kwargs)   
+        
+    # print(x,y,data['ZZ'])
+    return spectro_data
+
+# Convert spectroscopy data dictionary to dataframe for plotting and calculate parameter
+def wsxm_calcspectroparam(spectro_data, channel):
+    #perform calculations for parameters (e.g. adhesion, stiffness, check FUNC_DICT) on the single spectroscopy curve
+    # spectro_data = data[channel]['curves'][curv_num]['data']
+    df_spec = convert_spectro2df(spectro_data) #pd.DataFrame.from_dict(data_fd_dict) #for plotting
+    data_dict_param = {}
+    for param in FUNC_DICT[channel].keys():
+        # if channel == FUNC_DICT[param]['channel']:
+        kwargs = FUNC_DICT[channel][param]['kwargs']
+        _, data_dict_param[param] = FUNC_DICT[channel][param]['function'](spectro_data, **kwargs)
+        
+    return df_spec, data_dict_param
+
+#convert spectro data dictionary to dataframe for plotting
+def convert_spectro2df(data_dict):
+    data = {'x': np.empty(0), 'y': np.empty(0), 'segment': np.empty(0)}
+    for key, val in data_dict.items():
+        for k in val.keys():
+            data[k] = np.append(data[k], val[k])
+            data_len = len(val[k])
+        data['segment'] = np.append(data['segment'], [key]*data_len)        
+    df_spec = pd.DataFrame.from_dict(data)
+    return df_spec
+    
+#obtain property image from spectroscopy data of force-volume based on functions defined in FUNC_DICT
+def calc_spectro_prop(data, channel, img_dir):
+    # # img_keys = [key for key in data[channel].keys() if key.startswith(f'Image {img_dir}')] 
+    # data_dict_spectro = {}
+    # params = FUNC_DICT[channel].keys()
+    # for param in params:
+    #     data_dict_spectro[param] = np.empty(0)
+    # #get common data from first channel e.g. number of points
+    # # channel = FUNC_DICT[params[0]]['channel']
+    # key = list(data[channel].keys())[0]
+    # x_pts = int(data[channel][key]['header']['Number of rows'])
+    # y_pts = int(data[channel][key]['header']['Number of columns'])
+    # for y in range(y_pts):
+    #     for x in range(x_pts):
+    #         for param in params:
+    #             # channel = FUNC_DICT[channel][param]['channel']
+    #             kwargs = FUNC_DICT[channel][param]['kwargs']
+    #             img_keys = [key for key in data[channel].keys() if key.startswith(f'Image {img_dir}')]
+    #             spectro_data = {}
+    #             for key in img_keys:
+    #                 spectro_dir = SPECT_DICT[key.split(' ')[3]]
+    #                 spectro_data[spectro_dir] = {'y': data[channel][key]['data']['ZZ'][:,y,x],
+    #                                              'x': data[channel][key]['data']['Z']}
+    #             param_result,_ = FUNC_DICT[channel][param]['function'](spectro_data, **kwargs)
+    #             data_dict_spectro[param] = np.append(data_dict_spectro[param], [param_result])
+    # # data_dict_spectro[spectro_dir] = adh_data.reshape(x_pts,y_pts)
+    # for param in params:
+    #     data_dict_spectro[param] = data_dict_spectro[param].reshape(x_pts, y_pts)
+
+    # img_keys = [key for key in data[channel].keys() if key.startswith(f'Image {img_dir}')] 
+    data_dict_param = {}
+    chans = list(data.keys())
+    chans.remove('Topography')
+    for channel in chans:
+        params = FUNC_DICT[channel].keys()
+        for param in params:
+            data_dict_param[param] = {}
+            for chan_dir in data[channel].keys():
+                img_dir = chan_dir.split(' ')[1] #'Forward' or 'Backward'
+                if img_dir not in data_dict_param[param].keys():
+                    data_dict_param[param][img_dir] = {'data': {'X': data[channel][chan_dir]['data']['X'], 
+                                                                'Y':  data[channel][chan_dir]['data']['Y'], 
+                                                                'Z': np.empty(0)},
+                                                       'header':{}
+                                                      }
+                                                       
+            #get common data from first channel e.g. number of points
+            # channel = FUNC_DICT[params[0]]['channel']
+            # key = list(data[channel].keys())[0]
+                    x_pts = int(data[channel][chan_dir]['header']['Number of rows'])
+                    y_pts = int(data[channel][chan_dir]['header']['Number of columns'])
+                    for y in range(y_pts):
+                        for x in range(x_pts):
+                            # for param in params:
+                                # channel = FUNC_DICT[channel][param]['channel']
+                            kwargs = FUNC_DICT[channel][param]['kwargs']
+                            img_keys = [key for key in data[channel].keys() if key.startswith(f'Image {img_dir}')]
+                            spectro_data = {}
+                            for key in img_keys:
+                                spectro_dir = SPECT_DICT[key.split(' ')[3]]
+                                spectro_data[spectro_dir] = {'y': data[channel][key]['data']['ZZ'][:,y,x],
+                                                             'x': data[channel][key]['data']['Z']}
+                            param_result,_ = FUNC_DICT[channel][param]['function'](spectro_data, **kwargs)
+                            data_dict_param[param][img_dir]['data']['Z'] = np.append(data_dict_param[param][img_dir]['data']['Z'], [param_result])
+                # data_dict_spectro[spectro_dir] = adh_data.reshape(x_pts,y_pts)
+    for param in data_dict_param.keys():
+        for img_dir in data_dict_param[param].keys():
+            data_dict_param[param][img_dir]['data']['Z'] = data_dict_param[param][img_dir]['data']['Z'].reshape(x_pts, y_pts)    
+    return data_dict_param
+
+#get image data in appropriate matrix structure for plotting
+def get_imgdata(data_dict_chan, style = 'XY', x=0, y=0, z=0):
+    data0 = data_dict_chan['data'][style[0]]
+    data1 = data_dict_chan['data'][style[1]]
+    data_mat = np.meshgrid(data0, data1)
+    if 'ZZ' in data_dict_chan['data'].keys(): #for force volume data
+        if style == 'XY':
+            img_data = data_dict_chan['data']['ZZ'][z,:,:] #1st index:xy sections, 2nd index:xz sections, 3rd index: yz sections
+        elif style == 'XZ':
+            img_data = data_dict_chan['data']['ZZ'][:,y,:]
+        elif style == 'YZ':
+            img_data = data_dict_chan['data']['ZZ'][:,:,x]
+    else: #for usual image data
+        img_data = data_dict_chan['data']['Z']
+    data_mat.append(img_data)
+    return data_mat[0], data_mat[1], data_mat[2]
--- wsxm_read.py
+++ wsxm_read.py
@@ -73,7 +73,7 @@ def wsxm_readimg(file, header_dict, pos):
 
     x_data = np.linspace(x_len, 0, x_num, endpoint=True) #if x_dir == 'Backward' else np.linspace(x_len, 0, x_num, endpoint=True)
     y_data = np.linspace(0, y_len, y_num, endpoint=True) #if y_dir == 'Down' else np.linspace(y_len, 0, y_num, endpoint=True)
-    xx_data, yy_data = np.meshgrid(x_data, y_data)
+    # xx_data, yy_data = np.meshgrid(x_data, y_data)
     
     #read binary image data
     point_length, type_code  = DATA_TYPES[data_format]
@@ -89,9 +89,9 @@ def wsxm_readimg(file, header_dict, pos):
         z_calib = z_len/(ch_array.max()-ch_array.min())
     
     #img data dictionary
-    data_dict_chan = {'data': {'Z': ch_array*z_calib,
-                               'X': xx_data.flatten(),
-                               'Y':yy_data.flatten(),},
+    data_dict_chan = {'data': {'Z': z_calib*ch_array.reshape(x_num, y_num),
+                               'X': x_data,
+                               'Y': y_data},
                       'header': header_dict}
     
     pos += data_len #bytes read so far
@@ -162,12 +162,15 @@ def wsxm_readcurves(filepath):
                 file.seek(pos, 0)
                 bin_data = file.read(data_len)
                 ch_array = np.array(list(struct.iter_unpack(f'{type_code}', bin_data))).flatten()
+                x_data, y_data = np.split(ch_array[::2], 2), np.split(ch_array[1::2], 2)
                 
-                data_dict_curv[curv_ind] = {'data': {'x': ch_array[::2],
-                                                     'y': chan_offs+(ch_array[1::2]*chan_fact), #converted to proper units
-                                                     'segment':np.append(line_pts * [line_order[0]],line_pts * [line_order[1]])},
-                                            'header': header_dict
-                                            }
+                data_dict_curv[curv_ind] = {'header': header_dict, 'data': {}}
+                for i, curv_dir in enumerate(line_order):                    
+                    data_dict_curv[curv_ind]['data'][curv_dir] = {'x': x_data[i].max()-x_data[i], #reverse x data
+                                                                  'y': chan_offs+(y_data[i]*chan_fact) #converted to proper units
+                                                                  }
+                                                        # 'segment':np.append(line_pts * [line_order[0]],line_pts * [line_order[1]])},
+                                                          
                 
                 if curv_ind == curv_num:
                     break
@@ -175,8 +178,8 @@ def wsxm_readcurves(filepath):
                     pos += data_len #bytes read so far
                     file.seek(pos, 0)
         
-            data_dict[y_label] = {'Channel': data_dict_chan,
-                                  'Curves': data_dict_curv
+            data_dict[y_label] = {'image': data_dict_chan,
+                                  'curves': data_dict_curv
                                   }
             file.close()
     
@@ -191,14 +194,6 @@ def wsxm_readstp(filepath):
         if path_ext == '.stp': # read *.stp spectroscopy files
             file = open(f'{path}','rb')
             filename = os.path.basename(path)
-            # header_dict, pos = wsxm_readheader(file)
-            # data_dict_chan, pos = wsxm_readimg(file, header_dict, pos) 
-            
-            # data_format = header_dict['Image Data Type']
-            # point_length, type_code  = DATA_TYPES[data_format]
-            data_dict_curv = {}
-            
-            # file.seek(pos, 0)
             header_dict, pos = wsxm_readheader(file)
             data_format = header_dict['Image Data Type']
             chan_label = filename.split('_')[-1].split('.')[0] #header_dict['Acquisition channel']
@@ -214,9 +209,6 @@ def wsxm_readstp(filepath):
             chan_fact = float(header_dict['Conversion Factor 00'].split(' ')[0])
             chan_offs = float(header_dict['Conversion Offset 00'].split(' ')[0])
 
-            # x_data = np.linspace(x_len, 0, x_num, endpoint=True) #if x_dir == 'Backward' else np.linspace(x_len, 0, x_num, endpoint=True)
-            # y_data = np.linspace(0, y_len, y_num, endpoint=True) #if y_dir == 'Down' else np.linspace(y_len, 0, y_num, endpoint=True)
-            # xx_data, yy_data = np.meshgrid(x_data, y_data)
             z_data = np.linspace(x_len, 0, y_num, endpoint=True) #CHECK THIS
             
             #read binary image data
@@ -228,70 +220,23 @@ def wsxm_readstp(filepath):
             # print(data.read()[(x_num*y_num*point_length)+header_size:])
             ch_array = np.array(list(struct.iter_unpack(f'{type_code}', bin_data))).flatten() 
             ch_mat = ch_array.reshape(x_num,y_num)
-            # if z_len == 0: #for zero data
-            #     z_calib = 1
-            # else:
-            #     z_calib = z_len/(ch_array.max()-ch_array.min())
+            if z_len == 0: #for zero data
+                z_calib = 1
+            else:
+                z_calib = z_len/(ch_array.max()-ch_array.min())
         
-            # line_pts = int(header_dict['Number of points'])
-            # line_num = int(header_dict['Number of lines'])
-            # y_label = header_dict['Y axis text'].split('[')[0].strip()
-            # x_label = header_dict['X axis text'].split('[')[0].strip()
-            # curv_ind = int(header_dict['Index of this Curve'])
-            # curv_num = int(header_dict['Number of Curves in this serie'])
-            # #CHECK THIS FOR SECOND ARRAY! MAY NOT WORK FOR 3D Mode!
-            # # chan_adc2v = 1#20/2**16 #adc to volt converter for 20V DSP, 16 bit resolution
-            
-            # chan_fact = float(header_dict['Conversion Factor 00'].split(' ')[0])
-            # chan_offs = float(header_dict['Conversion Offset 00'].split(' ')[0])
-            
-            # aqpt_x, aqpt_y = tuple(map(float, header_dict['Acquisition point'].replace('nm','').
-            #                            replace('(','').replace(')','').split(',')))
-            # time_f = float(header_dict['Forward plot total time'].split(' ')[0])
-            # time_b = float(header_dict['Backward plot total time'].split(' ')[0])
-            
-            # line_order = ['approach', 'retract']
-            # if header_dict['First Forward'] == 'No': #CHECK THIS
-            #     line_order = ['retract', 'approach']
-    
-            # data_len = line_pts*line_num*2*point_length
-            # file.seek(pos, 0)
-            # bin_data = file.read(data_len)
-            # ch_array = np.array(list(struct.iter_unpack(f'{type_code}', bin_data))).flatten()
-
-            #data dictionary initialised in a consistant format (also check wsxm_readcurves())
-
             for i in range(x_num):
                 curv_ind = i + 1
+                #data dictionary initialised in a consistant format (also check wsxm_readcurves())
                 if chan_label not in data_dict.keys():
-                    data_dict[chan_label] = {'Curves': {}}
-                if curv_ind not in data_dict[chan_label]['Curves'].keys():
-                    data_dict[chan_label]['Curves'][curv_ind] = {'data': {'x': np.empty(0), 'y': np.empty(0), 'segment': np.empty(0)},
-                                                                 'header': header_dict
-                                                                }
-                
-                data_dict[chan_label]['Curves'][curv_ind]['data']['x'] = np.append(data_dict[chan_label]['Curves'][curv_ind]['data']['x'], 
-                                                                           z_data)
-                data_dict[chan_label]['Curves'][curv_ind]['data']['y'] = np.append(data_dict[chan_label]['Curves'][curv_ind]['data']['y'], 
-                                                                           chan_offs+(ch_mat[:][i]*chan_fact))
-                data_dict[chan_label]['Curves'][curv_ind]['data']['segment'] = np.append(data_dict[chan_label]['Curves'][curv_ind]['data']['segment'], 
-                                                                                 y_num*[z_dir])
-                # data_dict[chan_label]['Curves'][curv_ind]['header'] = header_dict #CHECK THIS. MAYBE CHANGE STRUCTURE OF DATA>separate direction
-                # data_dict[chan_label]['Curves'][curv_ind] = {'data': {'x': z_data,
-                #                                                       'y': chan_offs+(ch_mat[:][i]*chan_fact), #converted to proper units
-                #                                                       'segment':y_num*[z_dir]},
-                #                                              'header': header_dict
-                #                                              }
-            
-            # if curv_ind == curv_num:
-            #     break
-            # else:
-            #     pos += data_len #bytes read so far
-            #     file.seek(pos, 0)
-    
-            # data_dict[y_label] = {'Channel': data_dict_chan,
-            #                       'Curves': data_dict_curv
-            #                       }
+                    data_dict[chan_label] = {'curves': {}, 'image':{}}
+                if curv_ind not in data_dict[chan_label]['curves'].keys():
+                    data_dict[chan_label]['curves'][curv_ind] = {'data': {},'header': header_dict}
+                if z_dir not in data_dict[chan_label]['curves'][curv_ind]['data'].keys():
+                    data_dict[chan_label]['curves'][curv_ind]['data'][z_dir] = {}
+                data_dict[chan_label]['curves'][curv_ind]['data'][z_dir]['x'] = z_data
+                data_dict[chan_label]['curves'][curv_ind]['data'][z_dir]['y'] = z_calib*ch_mat[:][i] #chan_offs+(ch_mat[:][i]*chan_fact)
+
             file.close()
     
     return data_dict
@@ -324,11 +269,10 @@ def wsxm_readforcevol(filepath):
             chan_adc2v = float(header_dict['ADC to V conversion factor'].split(' ')[0])
             chan_fact = float(header_dict['Conversion factor 0 for input channel'].split(' ')[0])
             chan_offs = float(header_dict['Conversion offset 0 for input channel'].split(' ')[0])
-            
-        
+                    
             x_data = np.linspace(0, x_len, x_num, endpoint=True) if x_dir == 'Backward' else np.linspace(x_len, 0, x_num, endpoint=True)
             y_data = np.linspace(0, y_len, y_num, endpoint=True) if y_dir == 'Up' else np.linspace(y_len, 0, y_num, endpoint=True)
-            xx_data, yy_data = np.meshgrid(x_data, y_data)
+            # xx_data, yy_data = np.meshgrid(x_data, y_data)
         
             z_data = np.empty(0)
             for i in range(chan_num):
@@ -346,9 +290,9 @@ def wsxm_readforcevol(filepath):
             topo_array = np.array(list(struct.iter_unpack(f'{type_code}', bin_data))).flatten()
             topo_calib = z_len/(topo_array.max()-topo_array.min())
             #topo data dictionary
-            data_dict_topo = {'data': {'Z': topo_calib*topo_array,
-                                       'X': xx_data.flatten(),
-                                       'Y': yy_data.flatten()
+            data_dict_topo = {'data': {'Z': topo_calib*topo_array.reshape(x_num, y_num),
+                                       'X': x_data,
+                                       'Y': y_data
                                        },
                               'header': header_dict}
             topo_label = 'Topography'
@@ -373,8 +317,8 @@ def wsxm_readforcevol(filepath):
             
             #img data dictionary
             data_dict_chan = {'data': {'ZZ': ch_array.reshape(x_num,y_num,chan_num),
-                                       'X': xx_data.flatten(),
-                                       'Y': yy_data.flatten(),
+                                       'X': x_data,
+                                       'Y': y_data,
                                        'Z': z_data
                                       },
                               'header': header_dict}
