error bars plotted as bands in spectro dashboard; bug in normal deflection zero calculation fixed;unit calibration updated together with thermal/spectro calibration now

plot_funcs.py
@@ -126,15 +126,16 @@ def plotly_multiyplot(data, multiy_col, yvars, x, y, fig=None, yax_dict=None, li
     for yvars_i in yvars_new:
         data_i = data[data[multiy_col]==yvars_i].reset_index(drop=True)
         #include error data
-        if yvars_i in errory_dict.keys():
-            data_i['error'] = data[data[multiy_col]==errory_dict[yvars_i]][y].reset_index(drop=True)
-            error_y = 'error'
-        else:
-            error_y = None
+        # if yvars_i in errory_dict.keys():
+        #     data_i['error'] = data[data[multiy_col]==errory_dict[yvars_i]][y].reset_index(drop=True)
+        #     error_y = 'error'
+        # else:
+        #     error_y = None
             
         trace_i = px.line(data_i, x=x, y=y, line_group=line_group, symbol=symbol, color=color,
                           hover_name=hover_name, line_dash=line_dash, symbol_sequence = ['circle'],
-                          error_y=error_y, color_discrete_sequence=linecolor_list)
+                          error_y=None, color_discrete_sequence=linecolor_list)
+        
         if color == None: #follow y axis colors for lines
             trace_i.update_traces(yaxis=f'y{i+1}', line_color=color_list[i],# if color==None else None,
                                   showlegend = False, name='',
@@ -159,6 +160,24 @@ def plotly_multiyplot(data, multiy_col, yvars, x, y, fig=None, yax_dict=None, li
             fig.update_layout({f'yaxis{i+1}': dict(linecolor=yax_color,tickcolor=yax_color,
                                                    titlefont_color=yax_color,
                                                    tickfont_color=yax_color)})
+
+        # Add error band
+        if yvars_i in errory_dict.keys():
+            data_i['error'] = data[data[multiy_col]==errory_dict[yvars_i]][y].reset_index(drop=True)
+            fig.add_traces([
+                go.Scatter(
+                    x=np.concatenate([data_i[x], data_i[x][::-1]]),
+                    y=np.concatenate([data_i[y] + data_i['error'], (data_i[y] - data_i['error'])[::-1]]),
+                    fill='toself',
+                    fillcolor=color_list[i],
+                    opacity=0.5,
+                    line=dict(color='rgba(255,255,255,0)'), #invisible
+                    hoverinfo="skip",
+                    showlegend=False,
+                    name='Error band'
+                )
+            ])
+        
         yax_dict[yvars_i] = f'y{i+1}'
         i += 1
     

spectro_funcs.py
@@ -99,15 +99,17 @@ def snapin(defl_data, segment, method, min_percentile, fit_order, back_pts, find
         # # amp_sobel = ndimage.sobel(test_y) #sobel transform
         # # amp_sobel = ndimage.sobel(test_y_filt) #sobel transform
         # # ind_max = np.argmax(amp_sobel)
-        # try:
+        # try:       
         if ind_min == 0:
+            zero_y = np.median(data_y[:back_pts]) #median of inital "back_pts" number of points of data_y, ignores normal deflection drift here unlike others
             return {'value': 0, 'segment': segment, 'x': [], 'y': [], 'd':[], 'z': [],
-                    'index_min': 0, 'zero': 0}
+                    'index_min': 0, 'zero': zero_y}
         else:
             ind_maxs = np.where(data_y[:ind_min]>=np.percentile(data_y[:ind_min],min_percentile))[0]
             if len(ind_maxs) <= fit_order+1:
+                zero_y = np.median(data_y[:back_pts]) #median of inital "back_pts" number of points of data_y, ignores normal deflection drift here unlike others
                 return {'value': 0, 'segment': segment, 'x': [], 'y': [], 'd':[], 'z': [],
-                        'index_min': 0, 'zero': 0}
+                        'index_min': 0, 'zero': zero_y}
             else:
                 # # testmin_x, testmin_y = test_x[ind_max], test_y[ind_max]
                 # # poly = np.poly1d([-amp_sobel[ind_max], testmin_y-(-amp_sobel[ind_max]*testmin_x)])
@@ -152,9 +154,22 @@ def snapin(defl_data, segment, method, min_percentile, fit_order, back_pts, find
         if ind_max == 0 or findmax == False: #set findmax=False to find global minima in gradient and ignore max gradient value
             ind_max = len(data_y)
         ind_min = np.argmin(data_y_sobel[:ind_max]) #point of snapin
+        
+        # if back_pts > ind_min:
+        #     back_pts = ind_min
+        # if zero == 'max': #set method by which "zero deflection" point is estimated 
+        #     zero_y =  data_y[ind_min:ind_min-back_pts:-1].max() #max of back_pts before index of high gradient
+        # elif zero == 'mean':
+        #     zero_y =  np.mean(data_y[ind_min:ind_min-back_pts:-1]) #mean of back_pts before index of high gradient
+        # elif zero == 'median':
+        #     zero_y =  np.median(data_y[ind_min:ind_min-back_pts:-1]) #median of back_pts before index of high gradient
+        # elif zero == 'ini':
+        #     zero_y = np.median(data_y[:back_pts]) #median of inital "back_pts" number of points of data_y, ignores normal deflection drift here unlike others
+
         if ind_min == 0:
+            zero_y = np.median(data_y[:back_pts]) #median of inital "back_pts" number of points of data_y, ignores normal deflection drift here unlike others
             return {'value': 0, 'segment': segment, 'x': [], 'y': [], 'd':[], 'z': [], 
-                    'index_surf': 0, 'index_min': 0, 'zero': 0}
+                    'index_surf': 0, 'index_min': 0, 'zero': zero_y}
         else:
             if back_pts > ind_min:
                 back_pts = ind_min