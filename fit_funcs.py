cba6a0f6404cf6c7267a70f997d253b728bd61d5
Pranav Sudersan <pranavsudersan@gmail.com>
2025-06-18 19:36:45 +0200

    added van der waals fitting feature in spectroscopy dashboard

--- fit_funcs.py
+++ fit_funcs.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.special as sc
 
 #Lorentzian function
 # y0 = white noise offset, f0 = resonance freq, w = Full width at half maximum, A = area
@@ -27,3 +28,12 @@ def harmonic_amplitude(f, f0, gamma, F0, m):
 def sigmoid(x, L ,x0, k, b):
     y = L / (1 + np.exp(-k*(x-x0))) + b
     return (y)
+
+#analytical expression for frequency shift due to vdW interaction
+#reference: Giessibl and  Bielefeldt Phys. Rev. B 61, 9968 (https://doi.org/10.1103/PhysRevB.61.9968)
+def vdw_freqshift(z, C, z0, f0, k, A):
+    n = 2
+    d = z - z0
+    f_shift = (f0*C*np.sqrt(A)/(k*(A**(3/2))*(d**n)))*(sc.hyp2f1(n, 0.5, 1, -2*A/d)-sc.hyp2f1(n, 1.5, 2, -2*A/d))
+    # f_shift = (f0/(2*k))*(n*C/d**(n+1))
+    return f_shift
